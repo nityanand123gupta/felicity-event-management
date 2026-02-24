c28b552c83380a07eac660bf83b3a54eb5b11b12
nityanand123gupta <nityanand.gupta@students.iiit.ac.in>
2026-02-24 20:07:03 +0530

    Fix socket production URL

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,7 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL =
-  import.meta.env.VITE_SOCKET_URL || "http://localhost:8000";
+const SOCKET_URL = import.meta.env.VITE_API_URL.replace("/api", "");
 
 const socket = io(SOCKET_URL, {
   autoConnect: true,
